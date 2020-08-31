2dabd26319f5def9928d98c4f0e6c8035205bed4
Jerry <jchen123.862@gmail.com>
2020-08-31 20:35:52 +1200

    Updates

---
+++ precache-manifest.1948be870a1bc790c692613f21c0af56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e21c1397201f2499b3de8887fa60e6",
+    "url": "/quotes/index.html"
+  },
+  {
+    "revision": "f592235523c8f2fcd349",
+    "url": "/quotes/static/css/main.33f14d1b.chunk.css"
+  },
+  {
+    "revision": "d55ca209975ed60623a0",
+    "url": "/quotes/static/js/2.a358ccee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quotes/static/js/2.a358ccee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f592235523c8f2fcd349",
+    "url": "/quotes/static/js/main.90287279.chunk.js"
+  },
+  {
+    "revision": "42135f8f194da03e2725",
+    "url": "/quotes/static/js/runtime-main.8b2ab2e9.js"
+  }
+]);
